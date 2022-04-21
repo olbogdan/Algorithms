f50d8ad8c7678a55e974ef8040778f4038b8817c
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-04-22 00:43:08 +0200

    anagrams solution 2 based on filter()

--- AlgoBootcamp/Anagrams.py
+++ AlgoBootcamp/Anagrams.py
@@ -1,5 +1,20 @@
 import re
 
+def anagrams2(str1, str2):
+    plainStr1 = "".join(filter(lambda s: s.isalpha(), str1)).lower()
+    plainStr2 = "".join(filter(lambda s: s.isalpha(), str2)).lower()
+
+    dict1 = {}
+    for i in plainStr1:
+        dict1[i] = dict1.get(i, 0) + 1
+
+    dict2 = {}
+    for i in plainStr2:
+        dict2[i] = dict2.get(i, 0) + 1
+
+    return dict1 == dict2
+
+print(anagrams2("b aBx!", ",  babx"))
 
 def anagrams(str1, str2):
     plainStr1 = re.sub("[\W]", "", str1).lower()
@@ -21,9 +36,6 @@ def anagrams(str1, str2):
         else:
             dict2[i] = dict2[i] + 1
 
-    for key, value in dict1.items():
-        if key not in dict2 or dict2[key] != value:
-            return False
-    return True
+    return dict1 == dict2
 
 print(anagrams("b aBx!", ",  babx"))
