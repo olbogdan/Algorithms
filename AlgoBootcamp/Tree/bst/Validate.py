be105e8143eada3fdf3d532fbf3682b36e44b951
Oleksandr Bogdanov <7153849+olbogdan@users.noreply.github.com>
2022-05-12 01:21:43 +0200

    feat: validate a Binary Search Tree

---
+++ AlgoBootcamp/Tree/bst/Validate.py
@@ -0,0 +1,43 @@
+from AlgoBootcamp.Tree.bst.BinarySearchTree import Node
+
+def validate(node, down_constrain = float("-inf"), top_constrain = float("inf")):
+    if node.data < down_constrain or node.data > top_constrain:
+        return False
+    elif node.left is None and node.right is None:
+        return True
+
+    left_is_valid = True
+    if node.left:
+        left_is_valid = validate(node.left, down_constrain, node.data)
+    righ_is_valid = True
+    if node.right:
+        righ_is_valid = validate(node.right, node.data, top_constrain)
+
+    return left_is_valid and righ_is_valid
+
+
+node = Node(10)
+node.insert(20)
+node.insert(30)
+node.insert(5)
+node.insert(1)
+assert validate(node) == True
+
+node = Node(10)
+assert validate(node) == True
+
+node = Node(10)
+node.left = Node(20)
+assert validate(node) == False
+
+node = Node(10)
+node.right = Node(-1)
+assert validate(node) == False
+
+node = Node(10)
+node.insert(20)
+node.insert(30)
+node.insert(5)
+node.insert(1)
+node.left.left.data = 100
+assert validate(node) == False
