f432fd7f8932afbcd24ec526c8f05fee03b35645
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-05-12 10:57:38 +0200

    fix: validate a Binary Search Tree for duplicate items

--- AlgoBootcamp/Tree/bst/Validate.py
+++ AlgoBootcamp/Tree/bst/Validate.py
@@ -1,7 +1,7 @@
 from AlgoBootcamp.Tree.bst.BinarySearchTree import Node
 
 def validate(node, down_constrain = float("-inf"), top_constrain = float("inf")):
-    if node.data < down_constrain or node.data > top_constrain:
+    if node.data <= down_constrain or node.data >= top_constrain:
         return False
     elif node.left is None and node.right is None:
         return True
@@ -40,4 +40,9 @@ node.insert(30)
 node.insert(5)
 node.insert(1)
 node.left.left.data = 100
+assert validate(node) == False
+
+node = Node(2)
+node.right = Node(2)
+node.left = Node(2)
 assert validate(node) == False
