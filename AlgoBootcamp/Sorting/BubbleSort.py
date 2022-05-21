c5528c529d05e7c14c5aa8bcb17bbc57acd72f52
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-05-22 01:03:50 +0200

    test: more bubble sort test

--- AlgoBootcamp/Sorting/BubbleSort.py
+++ AlgoBootcamp/Sorting/BubbleSort.py
@@ -8,3 +8,7 @@ def sort(array):
 array = [-1, -2, 2, 1, 3, 4, 7, 5, 6, 6, 0, 10, 10, -18, -10]
 print(sort(array))
 assert sort(array) == [-18, -10, -2, -1, 0, 1, 2, 3, 4, 5, 6, 6, 7, 10, 10]
+
+array = [10, -1, -2]
+print(sort(array))
+assert sort(array) == [-2, -1, 10]
