191a538f65327de20a87028420d15e9714bad74e
Oleksandr Bogdanov <7153849+olbogdan@users.noreply.github.com>
2022-06-17 00:23:42 +0200

    refactor: two sum

--- AlgoBootcamp/leetcode/easy/1 two sum.py
+++ AlgoBootcamp/leetcode/easy/1 two sum.py
@@ -4,14 +4,13 @@ from typing import List
 def twoSum(nums: List[int], target: int) -> List[int]:
     hashNums = {}
     for i in range(0, len(nums)):
-        hashNums[nums[i]] = i
-    for i in range(0, len(nums)):
         num = nums[i]
         searchedNum = target - num
         if searchedNum in hashNums:
             index = hashNums[searchedNum]
             if index != i:
                 return [i, index]
+        hashNums[nums[i]] = i
 
 twoSum([2,7,11,15], 9) == [0,1]
 twoSum([3,2,4], 6) == [1,2]
