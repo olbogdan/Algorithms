04070c4ffeb52b6e57f0b91843cb02040fd3a407
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-06-28 00:47:55 +0200

    feat: simplified MinesweeperBombField

--- AlgoBootcamp/array/MinesweeperBombField.py
+++ AlgoBootcamp/array/MinesweeperBombField.py
@@ -1,8 +1,21 @@
 # create a minefield according to classincal minesweeper game rules
 from typing import List
 
-
 def minesweeper(bombs: List[List[int]], rows: int, columns: int) -> List[List[int]]:
+    result = [[0] * columns for _ in range(rows)]
+
+    for bomb in bombs:
+        row = bomb[0]
+        column = bomb[1]
+        result[row][column] = -1
+        for r in range(row-1, row+2):
+            for c in range(column-1, column+2):
+                if 0 <= r < rows and 0 <= c < columns and result[r][c] != -1:
+                    result[r][c] = result[r][c]+1
+    return result
+
+
+def minesweeper2(bombs: List[List[int]], rows: int, columns: int) -> List[List[int]]:
     result = [[0] * columns for _ in range(rows)]
 
     for bomb in bombs:
@@ -46,6 +59,8 @@ def minesweeper(bombs: List[List[int]], rows: int, columns: int) -> List[List[in
 result = minesweeper([[0, 2], [0, 3]], 3, 4)
 for row in result:
     print("".join(map(str, row)))
+
+print("\n")
 
 result = minesweeper([[0, 0], [10, 10], [15, 15], [15, 16], [19, 19]], 20, 20)
 for row in result:
