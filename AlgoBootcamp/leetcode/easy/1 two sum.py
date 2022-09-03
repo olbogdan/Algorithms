89f36d048d3e418fea5b5ac4542f19b0578f9a2e
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-09-03 23:48:28 +0200

    tests: 1 two sum

--- AlgoBootcamp/leetcode/easy/1 two sum.py
+++ AlgoBootcamp/leetcode/easy/1 two sum.py
@@ -8,10 +8,10 @@ def twoSum(nums: List[int], target: int) -> List[int]:
         searchedNum = target - num
         if searchedNum in hashNums:
             index = hashNums[searchedNum]
-            return [i, index]
+            return [index, i]
         hashNums[nums[i]] = i
 
-twoSum([2,7,11,15], 9) == [0,1]
-twoSum([3,2,4], 6) == [1,2]
-twoSum([3, 3], 6) == [0,1]
-twoSum([-1,-2,-3,-4,-5], -8) == [2,4]
+assert twoSum([2,7,11,15], 9) == [0,1]
+assert twoSum([3,2,4], 6) == [1,2]
+assert twoSum([3, 3], 6) == [0,1]
+assert twoSum([-1,-2,-3,-4,-5], -8) == [2,4]
