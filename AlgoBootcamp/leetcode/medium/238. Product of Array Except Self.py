f2e6196489c1da2eddb6dfd6736636f9168f250a
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-09-07 23:55:10 +0200

    feat: 238. Product of Array Except Self

--- AlgoBootcamp/leetcode/medium/238. Product of Array Except Self.py
+++ AlgoBootcamp/leetcode/medium/238. Product of Array Except Self.py
@@ -4,7 +4,7 @@
 #
 # You must write an algorithm that runs in O(n) time and without using the division operation.
 #
-
+# O(1) memory, result is not counted
 def productExceptSelf(nums: [int]) -> [int]:
     result = [1] * len(nums)
     product = 1
@@ -23,3 +23,29 @@ def productExceptSelf(nums: [int]) -> [int]:
     return result
 
 assert productExceptSelf([1, 2, 3, 4]) == [24,12,8,6]
+
+
+# O(n) memory
+def productExceptSelf(nums: [int]) -> [int]:
+    prefix = [1] * len(nums)
+    prev = 1
+    for i in range(len(nums)):
+        prefix[i] = nums[i] * prev
+        prev = prefix[i]
+
+    postfix = [1] * len(nums)
+    prev = 1
+    for i in range(len(nums) - 1, -1, -1):
+        postfix[i] = nums[i] * prev
+        prev = postfix[i]
+
+    result = [1] * len(nums)
+    for i in range(len(nums)):
+        if i == 0:
+            result[i] = postfix[i + 1]
+            continue
+        if i == len(nums) - 1:
+            result[i] = prefix[i - 1]
+            continue
+        result[i] = prefix[i - 1] * postfix[i + 1]
+    return result
