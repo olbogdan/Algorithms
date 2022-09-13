fedbedcad0098a028d2c37f80d06495b8c9c0aa2
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-09-13 20:06:27 +0200

    feat: 21. Merge Two Sorted Lists

--- AlgoBootcamp/leetcode/easy/21. Merge Two Sorted Lists.py
+++ AlgoBootcamp/leetcode/easy/21. Merge Two Sorted Lists.py
@@ -40,6 +40,7 @@ def mergeTwoLists(list1: Optional[Node], list2: Optional[Node]) -> Optional[Node
 
     return dummyNode.next
 
+
 n4 = Node(4, None)
 n3 = Node(3, n4)
 n2 = Node(1, n3)
@@ -48,6 +49,42 @@ sn2 = Node(12, None)
 sn1 = Node(0, sn2)
 
 result = mergeTwoLists(n1, sn1)
+assert result.data == 0
+assert result.next.data == 1
+assert result.next.next.data == 1
+assert result.next.next.next.data == 3
+assert result.next.next.next.next.data == 4
+assert result.next.next.next.next.next.data == 12
+
+
+def mergeTwoLists2(list1: Optional[Node], list2: Optional[Node]) -> Optional[Node]:
+    resultNode = Node(-1, None)
+    current = resultNode
+    while list1 and list2:
+        if list1.data < list2.data:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+
+    if list1:
+        current.next = list1
+    if list2:
+        current.next = list2
+
+    return resultNode.next
+
+
+n4 = Node(4, None)
+n3 = Node(3, n4)
+n2 = Node(1, n3)
+n1 = Node(1, n2)
+sn2 = Node(12, None)
+sn1 = Node(0, sn2)
+
+result = mergeTwoLists2(n1, sn1)
 assert result.data == 0
 assert result.next.data == 1
 assert result.next.next.data == 1
