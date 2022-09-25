2e3c4966fe9f2b0910ec5febc5ce61e6289ec7e6
Oleksandr Bogdanov <7153849+olbogdan@users.noreply.github.com>
2022-09-25 17:42:38 +0200

    feat: 23. Merge k Sorted Lists with mergesort

--- AlgoBootcamp/leetcode/hard/23. Merge k Sorted Lists.py
+++ AlgoBootcamp/leetcode/hard/23. Merge k Sorted Lists.py
@@ -70,3 +70,54 @@ assert result.next.next.val == 10
 assert result.next.next.next.val == 20
 assert result.next.next.next.next.val == 30
 assert result.next.next.next.next.next.val == 40
+
+
+def mergeKListsMergeSort(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    # divide and conquer
+    # divide list untill it will have 1 item
+    # take pairs of lists and merge sort them
+    # append result of each subsort to and array
+    # assign the colection of sub array to lists variable
+
+    if not lists:
+        return None
+    elif len(lists) == 1:
+        return lists[0]
+    elif len(lists) == 2:
+        return mergeSort(lists[0], lists[1])
+    else:
+        pivot = len(lists) // 2
+        list1 = mergeKLists(lists[0:pivot])
+        list2 = mergeKLists(lists[pivot:len(lists)])
+        return mergeSort(list1, list2)
+
+
+def mergeSort(l1, l2):
+    dummyNode = tail = ListNode(0, None)
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+    if l1:
+        tail.next = l1
+    if l2:
+        tail.next = l2
+    return dummyNode.next
+
+
+list1 = ListNode(5, ListNode(10, ListNode(20)))
+list2 = ListNode(1, ListNode(30, ListNode(40)))
+list3 = ListNode(100, ListNode(300, ListNode(400, ListNode(500))))
+list4 = None
+list5 = None
+result = mergeKListsMergeSort([list1, list2, list3, list4, list5])
+assert result.val == 1
+assert result.next.val == 5
+assert result.next.next.val == 10
+assert result.next.next.next.val == 20
+assert result.next.next.next.next.val == 30
+assert result.next.next.next.next.next.val == 40
