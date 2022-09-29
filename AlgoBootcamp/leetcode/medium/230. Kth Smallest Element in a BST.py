6cf76a29c4d717fef53fd7d8282d1a4e434ec37c
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-09-29 22:55:51 +0200

    feat: 230. Kth Smallest Element in a BST

---
+++ AlgoBootcamp/leetcode/medium/230. Kth Smallest Element in a BST.py
@@ -0,0 +1,34 @@
+# Given the root of a binary search tree, and an integer k,
+# return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+# Input: root = [3,1,4,null,2], k = 1
+# Output: 1
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+# Output: 3
+# Constraints:
+# The number of nodes in the tree is n.
+# 1 <= k <= n <= 104
+# 0 <= Node.val <= 104
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def kthSmallest(root: Optional[TreeNode], k: int) -> int:
+    array = []
+    def dfs(root):
+        if root.left:
+            dfs(root.left)
+        array.append(root.val)
+        if root.right:
+            dfs(root.right)
+    dfs(root)
+    return array[k - 1]
+
+
+assert kthSmallest(TreeNode(2, TreeNode(1), TreeNode(3)), 1) == 1
+assert kthSmallest(TreeNode(2, TreeNode(1), TreeNode(3)), 2) == 2
+assert kthSmallest(TreeNode(2, TreeNode(1), TreeNode(3)), 3) == 3
