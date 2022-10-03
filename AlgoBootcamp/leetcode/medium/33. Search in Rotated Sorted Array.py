29de9dc78148b3a3bab13653c8d91b91e7cafb49
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-10-03 21:35:39 +0200

    feat: 33. Search in Rotated Sorted Array

--- AlgoBootcamp/leetcode/medium/33. Search in Rotated Sorted Array.py
+++ AlgoBootcamp/leetcode/medium/33. Search in Rotated Sorted Array.py
@@ -1,4 +1,11 @@
-#
+#There is an integer array nums sorted in ascending order (with distinct values).
+# Prior to being passed to your function, nums is possibly rotated at an unknown
+# pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k],
+# nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+# Given the array nums after the possible rotation and an integer target,
+# return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
 
 def search(nums: [int], target: int) -> int:
     l, r = 0, len(nums) - 1
@@ -8,16 +15,20 @@ def search(nums: [int], target: int) -> int:
             return pivot
 
         if nums[l] <= nums[pivot]:
-            if target >= nums[l] and target < nums[pivot]:
+            if nums[l] <= target < nums[pivot]:
                 r = pivot - 1
             else:
                 l = pivot + 1
         else:
-            if target > nums[pivot] and target <= nums[r]:
+            if nums[pivot] < target <= nums[r]:
                 l = pivot + 1
             else:
                 r = pivot - 1
 
     return -1
 
-print(search([5,1,3], 5))
+
+assert search([5,1,3], 5) == 0
+assert search([4,5,6,7,0,1,2], 0) == 4
+assert search([4,5,6,7,0,1,2], 5) == 1
+assert search([4,5,6,7,0,1,2], 10) == -1
