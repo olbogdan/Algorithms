fffe914a4da7e6f56b47be68265f9303d3e0cfd4
Oleksandr Bogdanov <7153849+olbogdan@users.noreply.github.com>
2022-10-04 01:11:35 +0200

    feat: 208. trie prefix tree

---
+++ AlgoBootcamp/leetcode/medium/208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,55 @@
+# A trie (pronounced as "try") or prefix tree is a tree data structure used
+# to efficiently store and retrieve keys in a dataset of strings. There are
+# various applications of this data structure, such as autocomplete and spellchecker.
+# Implement the Trie class:
+#
+# Trie() Initializes the trie object.
+# void insert(String word) Inserts the string word into the trie.
+# boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+# boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+# Example 1:
+#
+# Input
+# ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+# [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+# Output
+# [null, null, true, false, true, null, true]
+#
+# Explanation
+# Trie trie = new Trie();
+# trie.insert("apple");
+# trie.search("apple");   // return True
+# trie.search("app");     // return False
+# trie.startsWith("app"); // return True
+# trie.insert("app");
+# trie.search("app");     // return True
+# Constraints:
+#
+# 1 <= word.length, prefix.length <= 2000
+# word and prefix consist only of lowercase English letters.
+# At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+
+class Trie:
+
+    def __init__(self):
+        self.words = set()
+        self.prefixes = set()
+
+    def insert(self, word: str) -> None:
+        self.words.add(word)
+
+        for i in range(len(word)):
+            self.prefixes.add(word[:i + 1])
+
+    def search(self, word: str) -> bool:
+        return word in self.words
+
+    def startsWith(self, prefix: str) -> bool:
+        return prefix in self.prefixes
+
+
+obj = Trie()
+obj.insert("apple")
+assert obj.search("apple") is True
+assert obj.startsWith("app") is True
+assert obj.startsWith("ple") is False
