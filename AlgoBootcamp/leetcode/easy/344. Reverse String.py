18d370baa23d274fd73b086290f9672b4ff26a56
Oleksandr Bogdanov <7153849+olbogdan@users.noreply.github.com>
2022-10-12 00:04:51 +0200

    feat: 344. Reverse String

--- AlgoBootcamp/leetcode/easy/344. Reverse String.py
+++ AlgoBootcamp/leetcode/easy/344. Reverse String.py
@@ -2,15 +2,27 @@
 #
 # You must do this by modifying the input array in-place with O(1) extra memory.
 
+
 def reverseString(s: [str]):
-    """
-    Do not return anything, modify s in-place instead.
-    """
+    l, r = 0, len(s) - 1
+    while l < r:
+        temp = s[l]
+        s[l] = s[r]
+        s[r] = temp
+        l += 1
+        r -= 1
+    return s
+
+
+assert reverseString(["a", "b"]) == ["b", "a"]
+assert reverseString(["a", "b", "c"]) == ["c", "b", "a"]
+
+def reverseString2(s: [str]):
     size = len(s)
     for i in range(0, size // 2):
         s[i], s[-i - 1] = s[-i - 1], s[i]
     return s
 
 
-assert reverseString(["a", "b"]) == ["b", "a"]
-assert reverseString(["a", "b", "c"]) == ["c", "b", "a"]
+assert reverseString2(["a", "b"]) == ["b", "a"]
+assert reverseString2(["a", "b", "c"]) == ["c", "b", "a"]
