6110d5ffe3281248243e55acb5e0d59fda768da0
Oleksandr Bogdanov <7153849+olbogdan@users.noreply.github.com>
2022-10-22 17:30:39 +0200

    feat: 703. Kth Largest Element in a Stream

---
+++ AlgoBootcamp/leetcode/easy/703. Kth Largest Element in a Stream.py
@@ -0,0 +1,78 @@
+# Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order,
+# not the kth distinct element.
+# Implement KthLargest class:
+#
+# KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+# int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element
+# in the stream.
+# Example 1:
+#
+# Input
+# ["KthLargest", "add", "add", "add", "add", "add"]
+# [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+# Output
+# [null, 4, 5, 5, 8, 8]
+#
+# Explanation
+# KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+# kthLargest.add(3);   // return 4
+# kthLargest.add(5);   // return 5
+# kthLargest.add(10);  // return 5
+# kthLargest.add(9);   // return 8
+# kthLargest.add(4);   // return 8
+# Constraints:
+#
+# 1 <= k <= 104
+# 0 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# -104 <= val <= 104
+# At most 104 calls will be made to add.
+# It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+from typing import List
+
+
+class KthLargest2:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.array = []
+        nums.sort()
+        self.array.extend(nums)
+        self.k = k
+
+    def add(self, val: int) -> int:
+        array = self.array
+        if len(array) == 0:
+            array.append(val)
+            return val
+        l, r = 0, len(array) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if array[mid] < val:
+                l = mid + 1
+            elif array[mid] > val:
+                r = mid - 1
+            else:
+                l = r = mid
+                break
+        # r can be eq or less than val
+        if r < 0:
+            r = 0
+        if array[r] == val:
+            array.insert(r, val)
+        elif array[r] < val:
+            # insert after r
+            if r + 1 < len(array):
+                array.insert(r + 1, val)
+            else:
+                array.append(val)
+        else:
+            array.insert(r, val)
+        return array[len(array) - self.k]
+
+
+obj = KthLargest2(2, [0])
+assert obj.add(-1) == -1
+assert obj.add(1) == 0
+assert obj.add(-2) == 0
+assert obj.add(-4) == 0
+assert obj.add(3) == 1
