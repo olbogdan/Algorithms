82aedb64f6feafbce0919e4132da9f5d0ad93979
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-10-30 21:40:04 +0100

    feat: 70. Climbing Stairs

---
+++ AlgoBootcamp/leetcode/easy/70. Climbing Stairs.py
@@ -0,0 +1,62 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+# Example 1:
+#
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+#
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+#
+# Constraints:
+#
+# 1 <= n <= 45
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cur = 1
+        prev = 1
+        for i in range(n-1):
+            newVal = cur + prev
+            prev = cur
+            cur = newVal
+        return cur
+
+
+sol = Solution()
+assert sol.climbStairs(3) == 3
+assert sol.climbStairs(4) == 5
+assert sol.climbStairs(10) == 89
+assert sol.climbStairs(1) == 1
+
+
+class Solution2:
+    def climbStairs(self, n: int) -> int:
+        arr = [1]*(n+1)
+        for i in range(n-1, -1, -1):
+            step = i + 1
+            twoSteps = i + 2
+            reachability = 0
+            if step < len(arr):
+                reachability += arr[step]
+            if twoSteps < len(arr):
+                reachability += arr[twoSteps]
+            arr[i] = reachability
+        return arr[0]
+
+
+sol = Solution2()
+assert sol.climbStairs(3) == 3
+assert sol.climbStairs(4) == 5
+assert sol.climbStairs(10) == 89
+assert sol.climbStairs(1) == 1
