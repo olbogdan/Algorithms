637de6af83ff5f582297980e5565ee62ac4ba067
Oleksandr Bogdanov <7153849+olbogdan@users.noreply.github.com>
2022-11-20 23:57:42 +0100

    feat: 853. Car Fleet

--- AlgoBootcamp/leetcode/medium/853. Car Fleet.py
+++ AlgoBootcamp/leetcode/medium/853. Car Fleet.py
@@ -5,7 +5,9 @@
 # where position[i] is the position of the ith car and speed[i] is the speed of the ith car (in miles per hour).
 #
 # A car can never pass another car ahead of it, but it can catch up to
-# it and drive bumper to bumper at the same speed. The faster car will slow down to match the slower car's speed. The distance between these two cars is ignored (i.e., they are assumed to have the same position).
+# it and drive bumper to bumper at the same speed.
+# The faster car will slow down to match the slower car's speed.
+# The distance between these two cars is ignored (i.e., they are assumed to have the same position).
 #
 # A car fleet is some non-empty set of cars driving at the same position
 # and same speed. Note that a single car is also a car fleet.
@@ -21,7 +23,8 @@
 # Explanation:
 # The cars starting at 10 (speed 2) and 8 (speed 4) become a fleet, meeting each other at 12.
 # The car starting at 0 does not catch up to any other car, so it is a fleet by itself.
-# The cars starting at 5 (speed 1) and 3 (speed 3) become a fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+# The cars starting at 5 (speed 1) and 3 (speed 3) become a fleet, meeting each other at 6.
+# The fleet moves at speed 1 until it reaches target.
 # Note that no other cars meet these fleets before the destination, so the answer is 3.
 # Example 2:
 #
@@ -34,7 +37,8 @@
 # Output: 1
 # Explanation:
 # The cars starting at 0 (speed 4) and 2 (speed 2) become a fleet, meeting each other at 4. The fleet moves at speed 2.
-# Then, the fleet (speed 2) and the car starting at 4 (speed 1) become one fleet, meeting each other at 6. The fleet moves at speed 1 until it reaches target.
+# Then, the fleet (speed 2) and the car starting at 4 (speed 1) become one fleet, meeting each other at 6. The fleet
+# moves at speed 1 until it reaches target.
 #
 #
 # Constraints:
