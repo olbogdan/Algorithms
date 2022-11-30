adb3b0b286cfda55104c6103b83c50e05d06ce90
bogdanov <iceposhta@gmail.com>
2022-12-01 00:29:29 +0100

    runtime optimization 66 plus one

--- AlgoBootcamp/leetcode/easy/66. Plus One.py
+++ AlgoBootcamp/leetcode/easy/66. Plus One.py
@@ -4,12 +4,25 @@
 # The large integer does not contain any leading 0's.
 #
 # Increment the large integer by one and return the resulting array of digits.
+# Input: digits = [9]
+# Output: [1,0]
+# Explanation: The array represents the integer 9.
+# Incrementing by one gives 9 + 1 = 10.
+# Thus, the result should be [1,0].
+# Constraints:
+#
+#     1 <= digits.length <= 100
+#     0 <= digits[i] <= 9
+#     digits does not contain any leading 0's.
 
 
 class Solution:
     def plusOne(self, digits: [int]) -> [int]:
         memo = 1
         for i in range(len(digits) - 1, -1, -1):
+            if memo == 0:
+                break
+
             num = digits[i]
             if num == 9 and memo:
                 num = 0
