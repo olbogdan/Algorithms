88c01f5a904e2e855ec699f267a3165a62d696ff
olbogdanov <iceposhta@gmail.com>
2023-01-24 23:26:55 +0000

    feat: 355. Design Twitter

---
+++ AlgoBootcamp/leetcode/medium/355. Design Twitter.py
@@ -0,0 +1,81 @@
+# Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
+#
+# Implement the Twitter class:
+#
+# Twitter() Initializes your twitter object.
+# void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. Each call to this function will be made with a unique tweetId.
+# List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user themself. Tweets must be ordered from most recent to least recent.
+# void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+# void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+# Example 1:
+#
+# Input
+# ["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+# [[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+# Output
+# [null, null, [5], null, null, [6, 5], null, [5]]
+#
+# Explanation
+# Twitter twitter = new Twitter();
+# twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+# twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+# twitter.follow(1, 2);    // User 1 follows user 2.
+# twitter.postTweet(2, 6); // User 2 posts a new tweet (id = 6).
+# twitter.getNewsFeed(1);  // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+# twitter.unfollow(1, 2);  // User 1 unfollows user 2.
+# twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.
+#
+#
+# Constraints:
+#
+# 1 <= userId, followerId, followeeId <= 500
+# 0 <= tweetId <= 104
+# All the tweets have unique IDs.
+# At most 3 * 104 calls will be made to postTweet, getNewsFeed, follow, and unfollow.
+from typing import List
+
+
+class Twitter:
+
+    def __init__(self):
+        self.followMap = {}
+        self.tweets = []
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets.append([tweetId, userId])
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        newTweets = []
+        followeeSet = self.followMap[userId] if userId in self.followMap else set()
+        for i in range(len(self.tweets)-1, -1, -1):
+            tweet = self.tweets[i]
+            if tweet[1] in followeeSet or tweet[1] == userId:
+                newTweets.append(tweet[0])
+            if len(newTweets) == 10:
+                return newTweets
+        return newTweets
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.followMap.keys():
+            self.followMap[followerId] = set()
+        self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.followMap.keys() and followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)
+            # del?
+
+
+obj = Twitter()
+obj.postTweet(1, 101)
+param_2 = obj.getNewsFeed(1)
+assert param_2 == [101]
+obj.follow(1, 2)
+obj.postTweet(2, 201)
+param_2 = obj.getNewsFeed(1)
+assert param_2 == [201, 101]
+obj.unfollow(1, 2)
+param_2 = obj.getNewsFeed(1)
+assert param_2 == [101]
+param_2 = obj.getNewsFeed(2)
+assert param_2 == [201]
