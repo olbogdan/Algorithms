bb4c22240aef83176a0c0b9048b3e453dc369ffe
olbogdanov <iceposhta@gmail.com>
2023-01-30 00:46:56 +0000

    feat: 695. Max Area of Island

---
+++ AlgoBootcamp/leetcode/medium/695. Max Area of Island.py
@@ -0,0 +1,48 @@
+# You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+#
+# The area of an island is the number of cells with a value 1 in the island.
+#
+# Return the maximum area of an island in grid. If there is no island, return 0.
+# Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+# Output: 6
+# Explanation: The answer is not 11, because the island must be connected 4-directionally.
+# Example 2:
+#
+# Input: grid = [[0,0,0,0,0,0,0,0]]
+# Output: 0
+#
+#
+# Constraints:
+#
+# m == grid.length
+# n == grid[i].length
+# 1 <= m, n <= 50
+# grid[i][j] is either 0 or 1.
+from typing import List
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        columns = len(grid[0])
+
+        visited = set()
+
+        def bfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= columns or grid[r][c] == 0 or (r, c) in visited:
+                return 0
+            visited.add((r, c))
+            return 1 + bfs(r + 1, c) + bfs(r - 1, c) + bfs(r, c + 1) + bfs(r, c - 1)
+
+        maxIsland = 0
+        for r in range(rows):
+            for c in range(columns):
+                if (r, c) not in visited:
+                    maxIsland = max(maxIsland, bfs(r, c))
+
+        return maxIsland
+
+
+sol = Solution()
+assert sol.maxAreaOfIsland([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]) == 6
+assert sol.maxAreaOfIsland([[0,0,0,0,0,0,0,0]]) == 0
