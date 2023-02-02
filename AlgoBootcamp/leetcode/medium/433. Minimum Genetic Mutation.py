14a4f4df68bb49cad0bdaef5bc08ae7c1a888015
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-02-02 12:33:54 +0000

    feat: 1071. Greatest Common Divisor of Strings

--- AlgoBootcamp/leetcode/medium/433. Minimum Genetic Mutation.py
+++ AlgoBootcamp/leetcode/medium/433. Minimum Genetic Mutation.py
@@ -23,30 +23,3 @@
 # 0 <= bank.length <= 10
 # startGene.length == endGene.length == bank[i].length == 8
 # startGene, endGene, and bank[i] consist of only the characters ['A', 'C', 'G', 'T'].
-
-
-class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1[0] != str2[0] or str1[-1] != str2[-1]:
-            return ""
-
-        if len(str2) > len(str1):
-            str1, str2 = str2, str1
-
-        divider = len(str2)
-        while divider > 0:
-            candidate2 = str2[0:divider] * (len(str1) // divider)
-            if str1 == candidate2:
-                return str2[0:divider]
-
-            divider -= 1
-            while divider > 1 and (len(str1) % divider != 0 or len(str2) % divider != 0):
-                divider -= 1
-        return ""
-
-
-sol = Solution()
-res = sol.gcdOfStrings("ABCABC", "ABC")
-assert res == "ABC"
-res = sol.gcdOfStrings("ABCABCA", "A")
-assert res == ""
