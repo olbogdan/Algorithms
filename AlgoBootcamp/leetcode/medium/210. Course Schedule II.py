091a235afa46473230b49da79e8b7b78f6084758
olbogdanov <iceposhta@gmail.com>
2023-02-05 23:22:03 +0000

    feat: 210. Course Schedule 2

---
+++ AlgoBootcamp/leetcode/medium/210. Course Schedule II.py
@@ -0,0 +1,109 @@
+# There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+#
+# For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+# Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+#
+#
+#
+# Example 1:
+#
+# Input: numCourses = 2, prerequisites = [[1,0]]
+# Output: [0,1]
+# Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+# Example 2:
+#
+# Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+# Output: [0,2,1,3]
+# Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
+# So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+# Example 3:
+#
+# Input: numCourses = 1, prerequisites = []
+# Output: [0]
+#
+#
+# Constraints:
+#
+# 1 <= numCourses <= 2000
+# 0 <= prerequisites.length <= numCourses * (numCourses - 1)
+# prerequisites[i].length == 2
+# 0 <= ai, bi < numCourses
+# ai != bi
+# All the pairs [ai, bi] are distinct.
+from typing import List
+
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        association = {i: [] for i in range(numCourses)}
+        for pair in prerequisites:
+            cource, prereq = pair[0], pair[1]
+            association[cource].append(prereq)
+
+        result = []
+        visited = set()
+        cycle = set()
+
+        def dfs(coursId):
+            if coursId in visited:
+                return True
+            if coursId in cycle:
+                return False
+            prereqs = association[coursId]
+            cycle.add(coursId)
+            for p in prereqs:
+                if not dfs(p):
+                    return False
+            cycle.remove(coursId)
+            result.append(coursId)
+            visited.add(coursId)
+            return True
+
+        for coursId in range(numCourses):
+            dfs(coursId)
+        if len(result) != numCourses:
+            return []
+        return result
+
+
+sol = Solution()
+res = sol.findOrder(2, [[1,0]])
+assert res == [0, 1]
+res = sol.findOrder(3, [[0,1],[0,2],[1,2]])
+assert res == [2, 1, 0]
+
+
+class Solution2:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        association = {i: [] for i in range(numCourses)}
+        for pair in prerequisites:
+            cource, prereq = pair[0], pair[1]
+            association[cource].append(prereq)
+
+        result = []
+
+        def dfs(coursId, visited):
+            if coursId in result:
+                return True
+            if coursId in visited:
+                return False
+            prereqs = association[coursId]
+            visited.add(coursId)
+            for p in prereqs:
+                if not dfs(p, visited):
+                    return False
+            result.append(coursId)
+            return True
+
+        for coursId in range(numCourses):
+            dfs(coursId, set())
+        if len(result) != numCourses:
+            return []
+        return result
+
+
+sol = Solution2()
+res = sol.findOrder(2, [[1,0]])
+assert res == [0, 1]
+res = sol.findOrder(3, [[0,1],[0,2],[1,2]])
+assert res == [2, 1, 0]
