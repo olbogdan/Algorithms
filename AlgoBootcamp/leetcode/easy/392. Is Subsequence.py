59e7c5254f2a228124cfa249f085f2b36db0153a
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-02-10 22:22:34 +0100

    feat: 392. Is Subsequence

--- AlgoBootcamp/leetcode/easy/392. Is Subsequence.py
+++ AlgoBootcamp/leetcode/easy/392. Is Subsequence.py
@@ -26,9 +26,41 @@
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        sL, sR = 0, len(s) - 1
+        tL, tR = 0, len(t) - 1
+        while tL <= tR:
+            if t[tL] == s[sL]:
+                sL += 1
+                tL += 1
+                continue
+            if t[tR] == s[sR]:
+                sR -= 1
+                tR -= 1
+                continue
+            if sL > sR:
+                return True
+            tL += 1
+            tR -= 1
+        return sL > sR
+
+
+sol = Solution()
+assert sol.isSubsequence("axc", "ahbxgdc") is True
+assert sol.isSubsequence("axc", "ahbgdc") is False
+
+
+class Solution2:
+    def isSubsequence(self, s: str, t: str) -> bool:
         i, j = 0, 0
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1
             j += 1
         return i == len(s)
+
+
+sol = Solution2()
+assert sol.isSubsequence("axc", "ahxbgdc") is True
+assert sol.isSubsequence("axc", "ahbgdc") is False
