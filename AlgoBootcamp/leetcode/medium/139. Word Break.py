e77bc1c7ae253194a9fe72755c2701164ffe9ba2
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-02-21 21:26:48 +0100

    feat: 139. Word Break

--- AlgoBootcamp/leetcode/medium/139. Word Break.py
+++ AlgoBootcamp/leetcode/medium/139. Word Break.py
@@ -29,3 +29,56 @@ def wordBreak(s: str, wordDict: List[str]) -> bool:
 assert wordBreak("aaaaaaa", ["aaaa", "aaa"])
 assert wordBreak("a", ["a"])
 assert wordBreak("leetcode", ["leetc", "leet", "cod", "e"])
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}  # index - Bool, True if can be reached
+
+        def dp(i):
+            if i == len(s):
+                return True
+            if i in memo:
+                return memo[i]
+
+            result = False
+            for w in wordDict:
+                # w is smaller than remaining len and == to s's substring
+                # [b, a(i)] compare to a(w), len(w)1+1 <= 2
+                if len(w) + i <= len(s) and s[i:len(w)+i] == w:
+                    if dp(i + len(w)):
+                        result = True
+            memo[i] = result
+            return result
+
+        return dp(0)
+
+
+sol = Solution()
+res = sol.wordBreak("leetcode", ["leet","code"])
+assert res is True
+
+
+class Solution2:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def isEquals(iStart, word) -> bool:
+            endOfW = len(word) + iStart
+            if endOfW <= len(s):
+                sW = s[iStart:endOfW]
+                return sW == word
+
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        for i in range(len(s)):
+            if dp[i]:
+                for w in wordDict:
+                    if isEquals(i, w):
+                        dp[i+len(w)] = True
+
+        return dp[-1]
+
+
+sol = Solution2()
+res = sol.wordBreak("leetcode", ["leet","code"])
+assert res is True
+
