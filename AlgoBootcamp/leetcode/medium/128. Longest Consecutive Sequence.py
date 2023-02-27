16f6ed6c2ab03a6736ce36c425646d1e93d68d16
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-02-27 13:03:08 +0000

    update: 128. Longest Consecutive Sequence

--- AlgoBootcamp/leetcode/medium/128. Longest Consecutive Sequence.py
+++ AlgoBootcamp/leetcode/medium/128. Longest Consecutive Sequence.py
@@ -4,27 +4,27 @@
 # Input: nums = [100,4,200,1,3,2]
 # Output: 4
 # Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
-
-def longestConsecutive(nums: [int]) -> int:
-    if len(nums) == 0:
-        return 0
-    collection = set(nums)
-
-    result = 1
-
-    for i in nums:
-        # detect start of a sequence
-        if i - 1 not in collection:
-            # found start of a sequence
-            tempresult = 0
-            sequenceI = i
-            while sequenceI in collection:
-                tempresult += 1
-                sequenceI += 1
-            result = max(result, tempresult)
-
-    return result
+# Constraints:
+#
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+from typing import List
 
 
-assert longestConsecutive([0,1,2,4,8,5,6,7,9,3,55,88,77,99,999999999]) == 10
-assert longestConsecutive([100,4,200,1,3,2]) == 4
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numsset = set(nums)
+        result = 0
+        for n in numsset:
+            # cur n is beginning of a sequence
+            if (n - 1) not in numsset:
+                r = 1
+                while (n + r) in numsset:
+                    r += 1
+                result = max(result, r)
+        return result
+
+
+sol = Solution()
+assert sol.longestConsecutive([0,1,2,4,8,5,6,7,9,3,55,88,77,99,999999999]) == 10
+assert sol.longestConsecutive([100,4,200,1,3,2]) == 4
