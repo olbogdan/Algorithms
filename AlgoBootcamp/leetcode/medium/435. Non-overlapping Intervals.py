27ef6d363013e327e25936d742d0de0910b17d99
olbogdanov <iceposhta@gmail.com>
2023-02-27 16:40:58 +0000

    update: 435. Non-overlapping Intervals

--- AlgoBootcamp/leetcode/medium/435. Non-overlapping Intervals.py
+++ AlgoBootcamp/leetcode/medium/435. Non-overlapping Intervals.py
@@ -5,12 +5,35 @@
 # Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
 # Output: 1
 # Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+from typing import List
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        resultIntervals = []
+        resultIntervals.append(intervals[0])
+        for i in range(1, len(intervals)):
+            prevEnd = resultIntervals[-1][1]
+            curStart = intervals[i][0]
+            if curStart < prevEnd:
+                resultIntervals[-1][1] = min(intervals[i][1], resultIntervals[-1][1])
+            else:
+                resultIntervals.append(intervals[i])
+        return len(intervals) - len(resultIntervals)
+
+
+sol = Solution()
+assert sol.eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]) == 1
+assert sol.eraseOverlapIntervals([[1,2],[1,2],[1,2]]) == 2
+assert sol.eraseOverlapIntervals([[1,2],[2,3]]) == 0
+assert sol.eraseOverlapIntervals([[1,2]]) == 0
+
 
 def eraseOverlapIntervals(intervals: [[int]]) -> int:
     intervals.sort(key=lambda x: x[1])
 
     maxCountNotOverlapping = 1
-    begin = intervals[0][0]
     end = intervals[0][1]
 
     for i in range(1, len(intervals)):
@@ -24,7 +47,7 @@ def eraseOverlapIntervals(intervals: [[int]]) -> int:
 
     return len(intervals) - maxCountNotOverlapping
 
-eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]) == 1
-eraseOverlapIntervals([[1,2],[1,2],[1,2]]) == 2
-eraseOverlapIntervals([[1,2],[2,3]]) == 0
-eraseOverlapIntervals([[1,2]]) == 0
+assert eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]) == 1
+assert eraseOverlapIntervals([[1,2],[1,2],[1,2]]) == 2
+assert eraseOverlapIntervals([[1,2],[2,3]]) == 0
+assert eraseOverlapIntervals([[1,2]]) == 0
