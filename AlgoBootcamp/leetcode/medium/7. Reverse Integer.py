cf508829b18347b75861021b43440bdfce2aaded
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-02-27 23:32:23 +0000

    update: 7. Reverse Integer

--- AlgoBootcamp/leetcode/medium/7. Reverse Integer.py
+++ AlgoBootcamp/leetcode/medium/7. Reverse Integer.py
@@ -32,10 +32,34 @@ class Solution:
         xStr = str(x)
         xStr = xStr[::-1]
         result = int(xStr) * lesThenZero
+        # 2**31 2147483648
         return result if -2**31 <= result <= 2**31 else 0
 
 
 sol = Solution()
+assert sol.reverse(1534236469) == 0
+assert sol.reverse(-123) == -321
+assert sol.reverse(-123) == -321
+assert sol.reverse(123) == 321
+
+
+class Solution2:
+    def reverse(self, x: int) -> int:
+        sign = 1
+        if x < 0:
+            sign = -1
+            x *= -1
+        result = 0
+        while x:
+            lastD = x % 10
+            x //= 10
+            result *= 10
+            result += lastD
+        result *= sign
+        return result if -2**31 <= result <= 2**31 else 0
+
+
+sol = Solution2()
 assert sol.reverse(1534236469) == 0
 assert sol.reverse(-123) == -321
 assert sol.reverse(-123) == -321
