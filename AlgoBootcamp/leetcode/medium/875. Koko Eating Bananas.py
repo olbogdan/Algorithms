67b87d0a68fa532fd747b6bf847d3f1d594cbe97
olbogdanov <iceposhta@gmail.com>
2023-03-08 22:11:00 +0000

    feat: 875. Koko Eating Bananas

--- AlgoBootcamp/leetcode/medium/875. Koko Eating Bananas.py
+++ AlgoBootcamp/leetcode/medium/875. Koko Eating Bananas.py
@@ -33,6 +33,41 @@ from typing import List
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # avrBananDay = sum(piles) // len(piles)
+        # timeMultiplier = len(piles) / h
+        # l = int(avrBananDay * timeMultiplier)
+        l = 1
+        r = max(piles)
+
+        def canEat(bananas):
+            hours = 0
+            for b in piles:
+                if b % bananas != 0:
+                    hours += (b//bananas + 1)
+                else:
+                    hours += b//bananas
+                if hours > h:
+                    return False
+            return hours <= h
+
+        while l < r:
+            mid = (l+r)//2
+            if canEat(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
+
+sol = Solution()
+assert sol.minEatingSpeed([312884470], 312884469) == 2
+assert sol.minEatingSpeed([3,6,7,11], 8) == 4
+assert sol.minEatingSpeed([312884470], 312884469) == 2
+assert sol.minEatingSpeed([1000000000,1000000000], 3) == 1000000000
+
+
+class Solution2:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
         left = int(sum(piles) / len(piles) / h)
         if left == 0:
             left = 1
@@ -58,7 +93,7 @@ class Solution:
         return result
 
 
-sol = Solution()
+sol = Solution2()
 assert sol.minEatingSpeed([3,6,7,11], 8) == 4
 assert sol.minEatingSpeed([312884470], 312884469) == 2
 assert sol.minEatingSpeed([1000000000,1000000000], 3) == 1000000000
