f656101de44a180548161583da30280c0138f8b1
olbogdanov <iceposhta@gmail.com>
2023-03-10 17:11:12 +0000

    feat: 97. Interleaving String, solution 2

--- AlgoBootcamp/leetcode/medium/97. Interleaving String.py
+++ AlgoBootcamp/leetcode/medium/97. Interleaving String.py
@@ -68,3 +68,28 @@ assert sol.isInterleave("aabcc", "dbbca", "aadbbcbcac") is True
 assert sol.isInterleave("a", "a", "aa") is True
 assert sol.isInterleave("", "", "") is True
 assert sol.isInterleave("", "", "d") is False
+
+
+class Solution2:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        @cache
+        def dp(i1, i2):
+            if i1 == len(s1) and i2 == len(s2):
+                return True
+
+            if i1 < len(s1) and s1[i1] == s3[i1 + i2] and dp(i1 + 1, i2):
+                return True
+            if i2 < len(s2) and s2[i2] == s3[i1 + i2] and dp(i1, i2 + 1):
+                return True
+            return False
+        return dp(0, 0)
+
+
+sol = Solution2()
+assert sol.isInterleave("aabcc", "dbbca", "aadbbcbcac") is True
+assert sol.isInterleave("a", "a", "aa") is True
+assert sol.isInterleave("", "", "") is True
+assert sol.isInterleave("", "", "d") is False
