4dd4b8988c5dd87c0b2cc6b9718dfedfa041c1ac
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-03-19 21:20:28 +0000

    feat: 721. Accounts Merge

---
+++ Kotlin/src/main/kotlin/leetcode/medium/721 Accounts Merge.kt
@@ -0,0 +1,97 @@
+package leetcode.medium
+/*Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+
+Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+
+After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+
+
+Example 1:
+
+Input: accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+Output: [["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+Explanation:
+The first and second John's are the same person as they have the common email "johnsmith@mail.com".
+The third John and Mary are different people as none of their email addresses are used by other accounts.
+We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'],
+['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+Example 2:
+
+Input: accounts = [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+Output: [["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
+
+
+Constraints:
+
+1 <= accounts.length <= 1000
+2 <= accounts[i].length <= 10
+1 <= accounts[i][j].length <= 30
+accounts[i][0] consists of English letters.
+accounts[i][j] (for j > 0) is a valid email.*/
+import kotlin.test.assertEquals
+
+class Solution {
+    fun accountsMerge(accounts: List<List<String>>): List<List<String>> {
+        // associate an email to user indexes, keyMail: list(idxs)
+        // { ab@bc.com : [1, 3, 4] }
+        val adj = mutableMapOf<String, MutableList<Int>>()
+        for ((idx, accs) in accounts.withIndex()) {
+            for (acIdx in 1 until accs.size) {
+                adj.getOrPut(accs[acIdx], { mutableListOf() }).add(idx)
+            }
+        }
+
+        val visited = mutableSetOf<Int>()
+        val result = mutableListOf<List<String>>()
+        for (idx in accounts.indices) {
+            if (idx in visited) {
+                continue
+            }
+            val resultSet = mutableSetOf<String>()
+            dfs(idx, accounts, adj, visited, resultSet)
+
+            val name = accounts[idx][0]
+            val resultList = resultSet.toList().sorted()
+            result.add(listOf(name) + resultList)
+        }
+        return result
+    }
+
+    private fun dfs(i: Int, accounts: List<List<String>>, adj: Map<String, List<Int>>,
+                    visited: MutableSet<Int>, resultSet: MutableSet<String>) {
+        if (i in visited) {
+            return
+        }
+        visited.add(i)
+        // iterate on all emails of i'th record
+        // ["John","c@m.com","b@m.com","a@m.com"]
+        // ignoring John 0 index name
+        val record = accounts[i]
+        for (emailIdx in 1 until record.size) {
+            val email = record[emailIdx]
+            resultSet.add(email)
+            // iterate on all users conencted to the email
+            for (nei in adj[email] ?: emptyList()) {
+                dfs(nei, accounts, adj, visited, resultSet)
+            }
+        }
+    }
+}
+
+fun main() {
+    val sol = Solution()
+    val res = sol.accountsMerge(listOf(
+        listOf("John", "johnsmith@mail.com", "john_newyork@mail.com"),
+        listOf("John", "johnsmith@mail.com", "john00@mail.com"),
+        listOf("Mary", "mary@mail.com"),
+        listOf("John", "johnnybravo@mail.com")
+    ))
+    val expected = listOf(
+        listOf("John", "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"),
+        listOf("Mary", "mary@mail.com"),
+        listOf("John", "johnnybravo@mail.com")
+    )
+    assertEquals(res, expected, "Test failed: got $res, expected $expected")
+}
+
