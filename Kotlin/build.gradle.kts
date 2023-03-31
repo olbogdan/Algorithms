a8c3855fc610820c6934be060dad85c4e607e18d
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-03-30 22:57:29 -0400

    add kotlinx extensions

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation(kotlin("test"))
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
