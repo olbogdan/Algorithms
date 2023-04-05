8ed70f12dab9255050645611299ef74fa69b16bd
olbogdanov <iceposhta@gmail.com>
2023-04-05 13:34:09 -0400

    feat: 179. Largest Number

---
+++ Kotlin/src/main/kotlin/leetcode/medium/179 Largest Number.kt
@@ -0,0 +1,33 @@
+package leetcode.medium
+
+/*Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+Since the result may be very large, so you need to return a string instead of an integer.
+
+
+
+Example 1:
+
+Input: nums = [10,2]
+Output: "210"
+Example 2:
+
+Input: nums = [3,30,34,5,9]
+Output: "9534330"
+
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 109*/
+import java.util.*
+class `179 Largest Number` {
+    fun largestNumber(nums: IntArray): String {
+        val strNums = nums.map { it.toString() }
+        val sortedNums = strNums.sortedWith(Comparator { n1, n2 ->
+            if (n1 + n2 > n2 + n1) -1 else 1
+        })
+        // handle [0, 0, 0] input
+        return sortedNums.joinToString("").let { if (it.startsWith('0')) "0" else it }
+    }
+}
