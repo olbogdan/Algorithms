dd65a3d5cb463a1242477794f256ea4ebad3b857
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-04-22 06:43:30 +0100

    feat: 879. Profitable Schemes

---
+++ Python/AlgoBootcamp/leetcode/hard/879. Profitable Schemes.py
@@ -0,0 +1,60 @@
+# There is a group of n members, and a list of various crimes they could commit. The ith crime generates a profit[i] and requires group[i] members to participate in it. If a member participates in one crime, that member can't participate in another crime.
+#
+# Let's call a profitable scheme any subset of these crimes that generates at least minProfit profit, and the total number of members participating in that subset of crimes is at most n.
+#
+# Return the number of schemes that can be chosen. Since the answer may be very large, return it modulo 109 + 7.
+#
+#
+#
+# Example 1:
+#
+# Input: n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+# Output: 2
+# Explanation: To make a profit of at least 3, the group could either commit crimes 0 and 1, or just crime 1.
+# In total, there are 2 schemes.
+# Example 2:
+#
+# Input: n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+# Output: 7
+# Explanation: To make a profit of at least 5, the group could commit any crimes, as long as they commit one.
+# There are 7 possible schemes: (0), (1), (2), (0,1), (0,2), (1,2), and (0,1,2).
+#
+#
+# Constraints:
+#
+# 1 <= n <= 100
+# 0 <= minProfit <= 100
+# 1 <= group.length <= 100
+# 1 <= group[i] <= 100
+# profit.length == group.length
+# 0 <= profit[i] <= 100
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        MOD = 10 ** 9 + 7
+        GROUP_SIZE = len(group)
+
+        @cache
+        def dp(idx, availableMembers, currentProfit):
+            if idx == GROUP_SIZE:
+                return 1 if currentProfit >= minProfit else 0
+
+            # skip the current group i
+            res = dp(idx + 1, availableMembers, currentProfit)
+
+            # take the current group i
+            if availableMembers >= group[idx]:
+                # reduce the number of states by taking min target instead of adding the profit
+                reducedProfit = min(currentProfit + profit[idx], minProfit)
+                res += dp(idx + 1, availableMembers - group[idx], reducedProfit)
+            return res % MOD
+
+        return dp(0, n, 0)
+
+
+sol = Solution()
+assert sol.profitableSchemes(5, 3, [2,2], [2,3]) == 2
+assert sol.profitableSchemes(10, 5, [2,3,5], [6,7,8]) == 7
