7786c1aeac401eb73047edae961c6368b650c776
olbogdanov <iceposhta@gmail.com>
2023-05-09 18:50:35 +0100

    feat: 54. Spiral Matrix

--- Python/AlgoBootcamp/leetcode/medium/54. Spiral Matrix.py
+++ Python/AlgoBootcamp/leetcode/medium/54. Spiral Matrix.py
@@ -1,9 +1,48 @@
 # Given an m x n matrix, return all elements of the matrix in spiral order.
 # Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 # Output: [1,2,3,6,9,8,7,4,5]
+from typing import List
 
 
 class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        firstRow = firstCol = 0
+        lastRow = len(matrix) - 1
+        lastCol = len(matrix[0]) - 1
+        result = []
+
+        while firstRow <= lastRow and firstCol <= lastCol:
+            for i in range(firstCol, lastCol + 1):
+                result.append(matrix[firstRow][i])
+            firstRow += 1
+
+            if firstRow > lastRow:
+                break
+
+            for i in range(firstRow, lastRow + 1):
+                result.append(matrix[i][lastCol])
+            lastCol -= 1
+
+            if firstCol > lastCol:
+                break
+
+            for i in reversed(range(firstCol, lastCol+1)):
+                result.append(matrix[lastRow][i])
+            lastRow -= 1
+
+            for i in reversed(range(firstRow, lastRow + 1)):
+                result.append(matrix[i][firstCol])
+            firstCol += 1
+
+        return result
+
+
+sol = Solution()
+assert sol.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]) == [1, 2, 3, 6, 9, 8, 7, 4, 5]
+assert sol.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]) == [1,2,3,4,8,12,11,10,9,5,6,7]
+
+
+class Solution2:
     def spiralOrder(self, matrix: [[int]]) -> [int]:
         left = top = 0
         right = len(matrix[0]) - 1
@@ -33,6 +72,6 @@ class Solution:
         return result
 
 
-sol = Solution()
+sol = Solution2()
 assert sol.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]) == [1, 2, 3, 6, 9, 8, 7, 4, 5]
 assert sol.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]) == [1,2,3,4,8,12,11,10,9,5,6,7]
