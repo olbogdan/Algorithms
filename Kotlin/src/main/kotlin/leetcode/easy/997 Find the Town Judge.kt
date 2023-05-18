56a9de05058a7a3b713fed06552e4bf1e95e1f5a
olbogdanov <iceposhta@gmail.com>
2023-05-18 11:59:26 +0100

    feat: 997. Find the Town Judge

---
+++ Kotlin/src/main/kotlin/leetcode/easy/997 Find the Town Judge.kt
@@ -0,0 +1,56 @@
+package leetcode.easy
+/*In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi. If a trust relationship does not exist in trust array, then such a trust relationship does not exist.
+
+Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+
+
+
+Example 1:
+
+Input: n = 2, trust = [[1,2]]
+Output: 2
+Example 2:
+
+Input: n = 3, trust = [[1,3],[2,3]]
+Output: 3
+Example 3:
+
+Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+
+
+Constraints:
+
+1 <= n <= 1000
+0 <= trust.length <= 104
+trust[i].length == 2
+All the pairs of trust are unique.
+ai != bi
+1 <= ai, bi <= n*/
+class `997 Find the Town Judge` {
+
+    fun findJudge(n: Int, trust: Array<IntArray>): Int {
+        val inDegree = HashMap<Int, Int>()
+        val outDegree = HashMap<Int, Int>()
+        for (t in trust) {
+            val a = t[0]
+            val b = t[1]
+            inDegree[b] = inDegree.getOrDefault(b, 0) + 1
+            outDegree[a] = outDegree.getOrDefault(a, 0) + 1
+        }
+        val judgeTrust = n - 1
+        for (person in 1..n) {
+            if (outDegree.getOrDefault(person, 0) == 0 && inDegree.getOrDefault(person, 0) == judgeTrust) {
+                return person
+            }
+        }
+        return -1
+    }
+}
