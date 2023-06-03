eac4e8826a3a8ce6803ba12a887417032649ad4d
olbogdanov <iceposhta@gmail.com>
2023-06-03 23:52:04 +0200

    feat: 225. Implement Stack using Queues

---
+++ Python/AlgoBootcamp/leetcode/easy/225. Implement Stack using Queues.py
@@ -0,0 +1,77 @@
+# Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+#
+# Implement the MyStack class:
+#
+# void push(int x) Pushes element x to the top of the stack.
+# int pop() Removes the element on the top of the stack and returns it.
+# int top() Returns the element on the top of the stack.
+# boolean empty() Returns true if the stack is empty, false otherwise.
+# Notes:
+#
+# You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+# Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+#
+#
+# Example 1:
+#
+# Input
+# ["MyStack", "push", "push", "top", "pop", "empty"]
+# [[], [1], [2], [], [], []]
+# Output
+# [null, null, null, 2, 2, false]
+#
+# Explanation
+# MyStack myStack = new MyStack();
+# myStack.push(1);
+# myStack.push(2);
+# myStack.top(); // return 2
+# myStack.pop(); // return 2
+# myStack.empty(); // return False
+#
+#
+# Constraints:
+#
+# 1 <= x <= 9
+# At most 100 calls will be made to push, pop, top, and empty.
+# All the calls to pop and top are valid.
+#
+#
+# Follow-up: Can you implement the stack using only one queue?
+from collections import deque
+
+
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+        self.topItem = None
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        self.topItem = x
+
+    def pop(self) -> int:
+        if self.empty():
+            return -1
+        N = len(self.queue)
+
+        for _ in range(N - 1):
+            self.push(self.queue.popleft())
+
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.topItem
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+
+# Your MyStack object will be instantiated and called as such:
+obj = MyStack()
+obj.push(12)
+assert obj.top() == 12
+obj.push(13)
+assert obj.top() == 13
+assert obj.pop() == 13
+assert obj.top() == 12
+assert obj.empty() is False
