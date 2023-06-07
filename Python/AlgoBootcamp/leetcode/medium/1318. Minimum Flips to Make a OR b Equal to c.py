4f3b05a57c1692cac72a244348ac28a213375790
olbogdanov <iceposhta@gmail.com>
2023-06-07 13:49:08 +0200

    feat: 1318. Minimum Flips to Make a OR b Equal to c

---
+++ Python/AlgoBootcamp/leetcode/medium/1318. Minimum Flips to Make a OR b Equal to c.py
@@ -0,0 +1,52 @@
+# Given 3 positives numbers a, b and c. Return the minimum flips required in some bits of a and b to make ( a OR b == c ). (bitwise OR operation).
+# Flip operation consists of change any single bit 1 to 0 or change the bit 0 to 1 in their binary representation.
+#
+#
+#
+# Example 1:
+#
+#
+#
+# Input: a = 2, b = 6, c = 5
+# Output: 3
+# Explanation: After flips a = 1 , b = 4 , c = 5 such that (a OR b == c)
+# Example 2:
+#
+# Input: a = 4, b = 2, c = 7
+# Output: 1
+# Example 3:
+#
+# Input: a = 1, b = 2, c = 3
+# Output: 0
+#
+#
+# Constraints:
+#
+# 1 <= a <= 10^9
+# 1 <= b <= 10^9
+# 1 <= c <= 10^9
+
+
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        result = 0
+        while a > 0 or b > 0 or c > 0:
+            bitA = a & 1
+            bitB = b & 1
+            bitC = c & 1
+
+            if bitC == 0:
+                result += bitA + bitB
+            elif bitC == 1:
+                if bitA == 0 and bitB == 0:
+                    result += 1
+
+            a = a >> 1
+            b = b >> 1
+            c = c >> 1
+
+        return result
+
+
+sol = Solution()
+assert sol.minFlips(2, 6, 5) == 3
