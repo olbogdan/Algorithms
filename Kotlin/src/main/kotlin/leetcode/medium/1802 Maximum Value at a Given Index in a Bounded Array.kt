fa43757c61e90d43ac483e1db59799eeae478c89
olbogdanov <iceposhta@gmail.com>
2023-06-10 18:02:48 +0200

    feat: 1802. Maximum Value at a Given Index in a Bounded Array

---
+++ Kotlin/src/main/kotlin/leetcode/medium/1802 Maximum Value at a Given Index in a Bounded Array.kt
@@ -0,0 +1,66 @@
+package leetcode.medium
+/*You are given three positive integers: n, index, and maxSum. You want to construct an array nums (0-indexed) that satisfies the following conditions:
+
+nums.length == n
+nums[i] is a positive integer where 0 <= i < n.
+abs(nums[i] - nums[i+1]) <= 1 where 0 <= i < n-1.
+The sum of all the elements of nums does not exceed maxSum.
+nums[index] is maximized.
+Return nums[index] of the constructed array.
+
+Note that abs(x) equals x if x >= 0, and -x otherwise.
+
+
+
+Example 1:
+
+Input: n = 4, index = 2,  maxSum = 6
+Output: 2
+Explanation: nums = [1,2,2,1] is one array that satisfies all the conditions.
+There are no arrays that satisfy all the conditions and have nums[2] == 3, so 2 is the maximum nums[2].
+Example 2:
+
+Input: n = 6, index = 1,  maxSum = 10
+Output: 3
+
+
+Constraints:
+
+1 <= n <= maxSum <= 109
+0 <= index < n*/
+class `1802 Maximum Value at a Given Index in a Bounded Array` {
+    fun maxValue(n: Int, index: Int, maxSum: Int): Int {
+        fun calcSum(cnt: Int, end: Int): Long {
+            if (cnt == 0) return 0
+            val start = maxOf(end - cnt, 0)
+            val sum1 = end.toLong() * (1 + end) / 2
+            val sum2 = start.toLong() * (1 + start) / 2
+            return sum1 - sum2
+        }
+
+        var maxSum = maxSum.toLong() - n
+        var l = 0L
+        var r = maxSum
+        while (l <= r) {
+            val mid = (l + r) / 2
+            val leftSum = calcSum(index + 1, mid.toInt())
+            val rightSum = calcSum(n - index - 1, mid.toInt() - 1)
+            if (leftSum + rightSum <= maxSum) {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+        return l.toInt()
+    }
+}
+
+fun main() {
+    val sol = `1802 Maximum Value at a Given Index in a Bounded Array`()
+    var res = sol.maxValue(4, 2, 6)
+    assert(res == 2)
+    res = sol.maxValue(6, 1, 10)
+    assert(res == 3)
+    res = sol.maxValue(1, 0, 6)
+    assert(res == 6)
+}
