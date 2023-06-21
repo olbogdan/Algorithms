b315636c7ddff64a51ba2c8f7ffde137b66f6d8f
olbogdanov <7153849+olbogdan@users.noreply.github.com>
2023-06-21 21:34:37 +0200

    feat: 2448. Minimum Cost to Make Array Equal

---
+++ Python/AlgoBootcamp/leetcode/hard/2448. Minimum Cost to Make Array Equal.py
@@ -0,0 +1,58 @@
+# You are given two 0-indexed arrays nums and cost consisting each of n positive integers.
+#
+# You can do the following operation any number of times:
+#
+# Increase or decrease any element of the array nums by 1.
+# The cost of doing one operation on the ith element is cost[i].
+#
+# Return the minimum total cost such that all the elements of the array nums become equal.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,3,5,2], cost = [2,3,1,14]
+# Output: 8
+# Explanation: We can make all the elements equal to 2 in the following way:
+# - Increase the 0th element one time. The cost is 2.
+# - Decrease the 1st element one time. The cost is 3.
+# - Decrease the 2nd element three times. The cost is 1 + 1 + 1 = 3.
+# The total cost is 2 + 3 + 3 = 8.
+# It can be shown that we cannot make the array equal with a smaller cost.
+# Example 2:
+#
+# Input: nums = [2,2,2,2,2], cost = [4,2,8,1,3]
+# Output: 0
+# Explanation: All the elements are already equal, so no operations are needed.
+#
+#
+# Constraints:
+#
+# n == nums.length == cost.length
+# 1 <= n <= 105
+# 1 <= nums[i], cost[i] <= 106
+from typing import List
+
+
+class Solution:
+    def minCost(self, nums: List[int], cost: List[int]) -> int:
+        N = len(nums)
+
+        def priceForX(x):
+            return sum(abs(nums[i] - x) * cost[i] for i in range(N))
+
+        low = min(nums) - 1
+        high = max(nums) + 1
+        while low < high:
+            mid = low + (high - low) // 2
+            if priceForX(mid) <= priceForX(mid + 1):
+                high = mid
+            else:
+                low = mid + 1
+        return priceForX(low)
+
+
+
+res = Solution().minCost([1,3,5,2], [2,3,1,14])
+assert res == 8
+
