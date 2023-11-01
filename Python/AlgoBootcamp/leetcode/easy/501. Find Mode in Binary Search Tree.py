f39f2bf2f7ef857305e9244584fe13d859a20ef8
Olek Bogdan <145340386+oleksandrbogdanovairalo@users.noreply.github.com>
2023-11-01 18:12:15 +0100

    feat: 501. Find Mode in Binary Search Tree

---
+++ Python/AlgoBootcamp/leetcode/easy/501. Find Mode in Binary Search Tree.py
@@ -0,0 +1,62 @@
+# Given the root of a binary search tree (BST) with duplicates, return all the mode(s) (i.e., the most frequently occurred element) in it.
+#
+# If the tree has more than one mode, return them in any order.
+#
+# Assume a BST is defined as follows:
+#
+# The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+# The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+# Both the left and right subtrees must also be binary search trees.
+#
+#
+# Example 1:
+#
+#
+# Input: root = [1,null,2,2]
+# Output: [2]
+# Example 2:
+#
+# Input: root = [0]
+# Output: [0]
+#
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [1, 104].
+# -105 <= Node.val <= 105
+#
+#
+# Follow up: Could you do that without using any extra space? (Assume that the implicit stack space incurred due to recursion does not count).
+from collections import defaultdict
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        count = defaultdict(int)
+        maxReps = 0
+        def dfs(node):
+            nonlocal maxReps
+            if not node:
+                return
+            count[node.val] += 1
+            maxReps = max(maxReps, count[node.val])
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        result = []
+        for k, v in count.items():
+            if v == maxReps:
+                result.append(k)
+        return result
+
+
+sol = Solution()
+res = sol.findMode(TreeNode(2))
+assert res == [2]
