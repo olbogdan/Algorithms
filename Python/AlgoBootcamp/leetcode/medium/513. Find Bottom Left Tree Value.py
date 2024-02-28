e2e496d95c1708523e39255ef01064f3b74a61fd
Olek Bogdan <7153849+olbogdan@users.noreply.github.com>
2024-02-28 23:40:03 +0100

    feat: 513. Find Bottom Left Tree Value

---
+++ Python/AlgoBootcamp/leetcode/medium/513. Find Bottom Left Tree Value.py
@@ -0,0 +1,48 @@
+# Given the root of a binary tree, return the leftmost value in the last row of the tree.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: root = [2,1,3]
+# Output: 1
+# Example 2:
+#
+#
+# Input: root = [1,2,3,4,null,5,6,null,null,7]
+# Output: 7
+#
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [1, 104].
+# -231 <= Node.val <= 231 - 1
+from collections import deque
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        result = root.val
+        dq = deque()
+        dq.append(root)
+        while dq:
+            n = len(dq)
+            for i in range(n):
+                node = dq.popleft()
+                if i == 0:
+                    result = node.val
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+        return result
