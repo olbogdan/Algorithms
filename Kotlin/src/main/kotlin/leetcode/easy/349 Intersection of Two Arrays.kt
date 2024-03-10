cb964793546f269d7a0b100b6a02df4e26e048f2
Olek Bogdan <7153849+olbogdan@users.noreply.github.com>
2024-03-10 12:18:33 +0100

    feat: 349 Intersection of Two Arrays

---
+++ Kotlin/src/main/kotlin/leetcode/easy/349 Intersection of Two Arrays.kt
@@ -0,0 +1,44 @@
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+//
+//
+//
+//Example 1:
+//
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2]
+//Example 2:
+//
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [9,4]
+//Explanation: [4,9] is also accepted.
+//
+//
+//Constraints:
+//
+//1 <= nums1.length, nums2.length <= 1000
+//0 <= nums1[i], nums2[i] <= 1000
+
+
+class `Intersection of Two Arrays` {
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        val n1Set = nums1.toSet()
+        val resultArray = nums2.toSet().filter { it in n1Set }.toTypedArray()
+        return resultArray.toIntArray()
+    }
+}
+
+class `Intersection of Two Arrays2` {
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        val setNums1 = nums1.toMutableSet()
+        val result = mutableListOf<Int>()
+
+        for (n in nums2) {
+            if (setNums1.contains(n)) {
+                result.add(n)
+                setNums1.remove(n)
+            }
+        }
+
+        return result.toIntArray()
+    }
+}
