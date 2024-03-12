f7a87c7d8b6b500223e9f2d60c856926a7d17d02
Olek Bogdan <7153849+olbogdan@users.noreply.github.com>
2024-03-12 18:59:32 +0100

    feat: 1171. Remove Zero Sum Consecutive Nodes from Linked List

---
+++ Python/AlgoBootcamp/leetcode/medium/1171. Remove Zero Sum Consecutive Nodes from Linked List.py
@@ -0,0 +1,60 @@
+# Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
+#
+# After doing so, return the head of the final linked list.  You may return any such answer.
+#
+#
+#
+# (Note that in the examples below, all sequences are serializations of ListNode objects.)
+#
+# Example 1:
+#
+# Input: head = [1,2,-3,3,1]
+# Output: [3,1]
+# Note: The answer [1,2,1] would also be accepted.
+# Example 2:
+#
+# Input: head = [1,2,3,-3,4]
+# Output: [1,2,4]
+# Example 3:
+#
+# Input: head = [1,2,3,-3,-2]
+# Output: [1]
+#
+#
+# Constraints:
+#
+# The given linked list will contain between 1 and 1000 nodes.
+# Each node in the linked list has -1000 <= node.val <= 1000.
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+        prefixes = {}  # {sum:node}
+        temp = dummy
+        total = 0
+
+        while temp:
+            total += temp.val
+            if total in prefixes:
+                removeNode = prefixes[total].next
+                currTotal = total + removeNode.val
+                while currTotal != total:
+                    del prefixes[currTotal]
+                    removeNode = removeNode.next
+                    currTotal += removeNode.val
+
+                prefixes[total].next = temp.next
+            else:
+                prefixes[total] = temp
+            temp = temp.next
+        return dummy.next
