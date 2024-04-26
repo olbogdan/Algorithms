7f9f0c6796f9dfe51007c16ce6d5ab623ec32eef
Olek Bogdan <7153849+olbogdan@users.noreply.github.com>
2024-04-26 21:10:10 +0200

    feat: 1289. minimum falling path sum 2

---
+++ Python/AlgoBootcamp/leetcode/hard/1289. Minimum Falling Path Sum II.py
@@ -0,0 +1,59 @@
+# Given an n x n integer matrix grid, return the minimum sum of a falling path with non-zero shifts.
+#
+# A falling path with non-zero shifts is a choice of exactly one element from each row of grid such that no two elements chosen in adjacent rows are in the same column.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: grid = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: 13
+# Explanation:
+# The possible falling paths are:
+# [1,5,9], [1,5,7], [1,6,7], [1,6,8],
+# [2,4,8], [2,4,9], [2,6,7], [2,6,8],
+# [3,4,8], [3,4,9], [3,5,7], [3,5,9]
+# The falling path with the smallest sum is [1,5,7], so the answer is 13.
+# Example 2:
+#
+# Input: grid = [[7]]
+# Output: 7
+#
+#
+# Constraints:
+#
+# n == grid.length == grid[i].length
+# 1 <= n <= 200
+# -99 <= grid[i][j] <= 99
+from typing import List
+
+
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        def find2Mins(row: List[int]):
+            res = []  # (val, idx) in increasing order
+            for i, val in enumerate(row):
+                if len(res) < 2:
+                    res.append((val, i))
+                    res.sort()
+                elif res[-1][0] > val:
+                    res.pop()
+                    res.append((val, i))
+                    res.sort()
+            return res
+
+        for r in range(1, len(grid)):
+            twoMin = find2Mins(grid[r - 1])
+            for c in range(len(grid[0])):
+                if twoMin[0][1] != c:  # index != cur column index
+                    grid[r][c] += twoMin[0][0]
+                else:
+                    grid[r][c] += twoMin[1][0]
+
+        return min(grid[-1])
+
+
+sol = Solution()
+assert sol.minFallingPathSum([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 13
+assert sol.minFallingPathSum([[7]]) == 7
