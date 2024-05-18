9cec6e000c61a947c2b6724d2ac655af22eb9cb8
Olek Bogdan <7153849+olbogdan@users.noreply.github.com>
2024-05-18 16:49:05 +0200

    feat: 814. Binary Tree Pruning

---
+++ Python/AlgoBootcamp/leetcode/medium/814. Binary Tree Pruning.py
@@ -0,0 +1,58 @@
+# Given the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+#
+# A subtree of a node node is node plus every node that is a descendant of node.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: root = [1,null,0,0,1]
+# Output: [1,null,0,null,1]
+# Explanation:
+# Only the red nodes satisfy the property "every subtree not containing a 1".
+# The diagram on the right represents the answer.
+# Example 2:
+#
+#
+# Input: root = [1,0,1,0,0,0,1]
+# Output: [1,null,1,null,1]
+# Example 3:
+#
+#
+# Input: root = [1,1,0,1,1,0,1,0]
+# Output: [1,1,0,1,1,null,1]
+#
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [1, 200].
+# Node.val is either 0 or 1.
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root.left:
+            root.left = self.pruneTree(root.left)
+        if root.right:
+            root.right = self.pruneTree(root.right)
+        if not root.left and not root.right and root.val != 1:
+            return None
+        return root
+
+
+sol = Solution()
+root = TreeNode(1)
+root.right = TreeNode(0)
+root.right.left = TreeNode(0)
+root.right.right = TreeNode(1)
+assert sol.pruneTree(root) == root
