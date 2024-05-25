9638dc2a176c9c2d1ec1a824a2639ab4f6d18040
Olek Bogdan <7153849+olbogdan@users.noreply.github.com>
2024-05-26 00:53:22 +0200

    feat: 140. Word Break II

---
+++ Python/AlgoBootcamp/leetcode/hard/140. Word Break II.py
@@ -0,0 +1,55 @@
+# Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+#
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+# Output: ["cats and dog","cat sand dog"]
+# Example 2:
+#
+# Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+# Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+# Explanation: Note that you are allowed to reuse a dictionary word.
+# Example 3:
+#
+# Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+# Output: []
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 20
+# 1 <= wordDict.length <= 1000
+# 1 <= wordDict[i].length <= 10
+# s and wordDict[i] consist of only lowercase English letters.
+# All the strings of wordDict are unique.
+# Input is generated in a way that the length of the answer doesn't exceed 105.
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        temp = []
+        wordDict = set(wordDict)
+
+        def helper(i):
+            if i == len(s):
+                res.append(" ".join(temp))
+                return
+            for k in range(i, len(s)):
+                word = s[i:k + 1]
+                if word in wordDict:
+                    temp.append(word)
+                    helper(k + 1)
+                    temp.pop()
+
+        helper(0)
+        return res
+
+
+sol = Solution()
+assert sol.wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"]) == ["cat sand dog", "cats and dog"]
