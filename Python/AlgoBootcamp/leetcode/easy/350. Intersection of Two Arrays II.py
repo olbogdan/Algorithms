e21cff0ee4882d1bf24467259a47c14e71a45e2e
Olek Bogdan <7153849+olbogdan@users.noreply.github.com>
2024-07-02 23:07:50 +0200

    feat: 350. Intersection of Two Arrays II

---
+++ Python/AlgoBootcamp/leetcode/easy/350. Intersection of Two Arrays II.py
@@ -0,0 +1,49 @@
+# Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+#
+#
+#
+# Example 1:
+#
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2,2]
+# Example 2:
+#
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [4,9]
+# Explanation: [9,4] is also accepted.
+#
+#
+# Constraints:
+#
+# 1 <= nums1.length, nums2.length <= 1000
+# 0 <= nums1[i], nums2[i] <= 1000
+#
+#
+# Follow up:
+#
+# What if the given array is already sorted? How would you optimize your algorithm?
+# What if nums1's size is small compared to nums2's size? Which algorithm is better?
+# What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2):
+            nums2, nums1 = nums1, nums2
+        # nums1 is smaller now
+        count = Counter(nums1)
+        result = []
+        for n in nums2:
+            if n in count:
+                result.append(n)
+                count[n] -= 1
+                if count[n] == 0:
+                    del count[n]
+        return result
+
+
+s = Solution()
+assert s.intersect([1, 2, 2, 1], [2, 2]) == [2, 2]
+assert s.intersect([4, 9, 5], [9, 4, 9, 8, 4]) == [9, 4]
