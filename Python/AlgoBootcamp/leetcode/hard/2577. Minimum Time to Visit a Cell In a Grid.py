c97f6de61d4463e39261c32ea92b551afe794e0d
Olek Bogdan <7153849+olbogdan@users.noreply.github.com>
2024-11-30 00:49:02 +0100

    feat: 2577. Minimum Time to Visit a Cell In a Grid

---
+++ Python/AlgoBootcamp/leetcode/hard/2577. Minimum Time to Visit a Cell In a Grid.py
@@ -0,0 +1,81 @@
+# You are given a m x n matrix grid consisting of non-negative integers where grid[row][col] represents the minimum time required to be able to visit the cell (row, col), which means you can visit the cell (row, col) only when the time you visit it is greater than or equal to grid[row][col].
+#
+# You are standing in the top-left cell of the matrix in the 0th second, and you must move to any adjacent cell in the four directions: up, down, left, and right. Each move you make takes 1 second.
+#
+# Return the minimum time required in which you can visit the bottom-right cell of the matrix. If you cannot visit the bottom-right cell, then return -1.
+#
+#
+#
+# Example 1:
+#
+#
+#
+# Input: grid = [[0,1,3,2],[5,1,2,5],[4,3,8,6]]
+# Output: 7
+# Explanation: One of the paths that we can take is the following:
+# - at t = 0, we are on the cell (0,0).
+# - at t = 1, we move to the cell (0,1). It is possible because grid[0][1] <= 1.
+# - at t = 2, we move to the cell (1,1). It is possible because grid[1][1] <= 2.
+# - at t = 3, we move to the cell (1,2). It is possible because grid[1][2] <= 3.
+# - at t = 4, we move to the cell (1,1). It is possible because grid[1][1] <= 4.
+# - at t = 5, we move to the cell (1,2). It is possible because grid[1][2] <= 5.
+# - at t = 6, we move to the cell (1,3). It is possible because grid[1][3] <= 6.
+# - at t = 7, we move to the cell (2,3). It is possible because grid[2][3] <= 7.
+# The final time is 7. It can be shown that it is the minimum time possible.
+# Example 2:
+#
+#
+#
+# Input: grid = [[0,2,4],[3,2,1],[1,0,4]]
+# Output: -1
+# Explanation: There is no path from the top left to the bottom-right cell.
+#
+#
+# Constraints:
+#
+# m == grid.length
+# n == grid[i].length
+# 2 <= m, n <= 1000
+# 4 <= m * n <= 105
+# 0 <= grid[i][j] <= 105
+# grid[0][0] == 0
+from heapq import heappop, heappush
+from math import inf
+from typing import List
+
+
+class Solution:
+    def minimumTime(self, grid: List[List[int]]) -> int:
+        if min(grid[0][1], grid[1][0]) > 1:
+            return -1
+        R, C = len(grid), len(grid[0])
+        visit = [[False] * C for _ in range(R)]
+        heap = []
+        heap.append((0, 0, 0))  # time, r, c
+        while heap:
+            time, r, c = heappop(heap)
+            if r == R - 1 and c == C - 1:
+                return time
+            dirs = [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]
+            for neiR, neiC in dirs:
+                if min(neiR, neiC) < 0 or neiR == R or neiC == C or visit[neiR][neiC]:
+                    continue
+                nextTime = time + 1
+                if nextTime < grid[neiR][neiC]:
+                    dif = grid[neiR][neiC] - nextTime
+                    if dif % 2:
+                        nextTime = grid[neiR][neiC] + 1
+                    else:
+                        nextTime = grid[neiR][neiC]
+
+                heappush(heap, (nextTime, neiR, neiC))
+                visit[neiR][neiC] = True
+
+        # invalid case -> exception
+        return -1
+
+
+sol = Solution()
+assert sol.minimumTime([[0,1,3,2],[5,1,2,5],[4,3,8,6]]) == 7
+assert sol.minimumTime([[0,2,4],[3,2,1],[1,0,4]]) == -1
+assert sol.minimumTime([[0,2,4],[3,2,1],[1,0,4],[1,0,4]]) == -1
