3d2ad8994989e7e6f20ee14b593ed2ec59c06f58
Alexander Bogdan <7153849+olbogdan@users.noreply.github.com>
2025-05-03 21:54:59 +0200

    feat: 1007. Minimum Domino Rotations For Equal Row

---
+++ Python/AlgoBootcamp/leetcode/medium/1007. Minimum Domino Rotations For Equal Row.py
@@ -0,0 +1,58 @@
+# In a row of dominoes, tops[i] and bottoms[i] represent the top and bottom halves of the ith domino. (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
+#
+# We may rotate the ith domino, so that tops[i] and bottoms[i] swap values.
+#
+# Return the minimum number of rotations so that all the values in tops are the same, or all the values in bottoms are the same.
+#
+# If it cannot be done, return -1.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: tops = [2,1,2,4,2,2], bottoms = [5,2,6,2,3,2]
+# Output: 2
+# Explanation:
+# The first figure represents the dominoes as given by tops and bottoms: before we do any rotations.
+# If we rotate the second and fourth dominoes, we can make every value in the top row equal to 2, as indicated by the second figure.
+# Example 2:
+#
+# Input: tops = [3,5,1,2,3], bottoms = [3,6,3,3,4]
+# Output: -1
+# Explanation:
+# In this case, it is not possible to rotate the dominoes to make one row of values equal.
+#
+#
+# Constraints:
+#
+# 2 <= tops.length <= 2 * 104
+# bottoms.length == tops.length
+# 1 <= tops[i], bottoms[i] <= 6
+from typing import List
+
+
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        N = len(tops)
+        def countOfNum(num):
+            countT = 0
+            countB = 0
+            for i in range(N):
+                if tops[i] != num and bottoms[i] != num:
+                    return -1
+                if tops[i] != num:
+                    countT += 1
+                if bottoms[i] != num:
+                    countB += 1
+            return min(countT, countB)
+
+        first = countOfNum(tops[0])
+        if first != -1:
+            return first
+        return countOfNum(bottoms[0])
+
+
+sol = Solution()
+assert sol.minDominoRotations([2, 1, 2, 4, 2, 2], [5, 2, 6, 2, 3, 2]) == 2
+assert sol.minDominoRotations([1,2,1,1,1,2,2,2], [2,1,2,2,2,2,2,2]) == 1
