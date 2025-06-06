d13ed923f682c3bf865027122ae48c78f7802af4
Alexander Bogdan <7153849+olbogdan@users.noreply.github.com>
2025-06-07 01:25:37 +0200

    feat: 2434. Using a Robot to Print the Lexicographically Smallest String

---
+++ Python/AlgoBootcamp/leetcode/medium/2434. Using a Robot to Print the Lexicographically Smallest String.py
@@ -0,0 +1,64 @@
+# You are given a string s and a robot that currently holds an empty string t. Apply one of the following operations until s and t are both empty:
+#
+# Remove the first character of a string s and give it to the robot. The robot will append this character to the string t.
+# Remove the last character of a string t and give it to the robot. The robot will write this character on paper.
+# Return the lexicographically smallest string that can be written on the paper.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "zza"
+# Output: "azz"
+# Explanation: Let p denote the written string.
+# Initially p="", s="zza", t="".
+# Perform first operation three times p="", s="", t="zza".
+# Perform second operation three times p="azz", s="", t="".
+# Example 2:
+#
+# Input: s = "bac"
+# Output: "abc"
+# Explanation: Let p denote the written string.
+# Perform first operation twice p="", s="c", t="ba".
+# Perform second operation twice p="ab", s="c", t="".
+# Perform first operation p="ab", s="", t="c".
+# Perform second operation p="abc", s="", t="".
+# Example 3:
+#
+# Input: s = "bdda"
+# Output: "addb"
+# Explanation: Let p denote the written string.
+# Initially p="", s="bdda", t="".
+# Perform first operation four times p="", s="", t="bdda".
+# Perform second operation four times p="addb", s="", t="".
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 105
+# s consists of only English lowercase letters.
+from collections import Counter
+
+
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        count = Counter(s)
+
+        def getMinChar():
+            for i in range(ord('a'), ord('z') + 1):
+                if count[chr(i)] > 0:
+                    return chr(i)
+            return chr(200)
+
+        temp = []
+        res = []
+        for char in s:
+            temp.append(char)
+            count[char] -= 1
+            while temp and temp[-1] <= getMinChar():
+                res.append(temp.pop())
+        return "".join(res)
+
+
+sol = Solution()
+assert sol.robotWithString("bydizfve") == "bdevfziy"
