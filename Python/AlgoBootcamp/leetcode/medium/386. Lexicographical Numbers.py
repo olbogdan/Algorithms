4f2ccc3e7de699566f738942553eb74573432b14
Alexander Bogdan <7153849+olbogdan@users.noreply.github.com>
2025-06-08 13:54:25 +0200

    refactor: 386. Lexicographical Numbers

--- Python/AlgoBootcamp/leetcode/medium/386. Lexicographical Numbers.py
+++ Python/AlgoBootcamp/leetcode/medium/386. Lexicographical Numbers.py
@@ -23,18 +23,42 @@ from typing import List
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         res = []
-        i = 1
+        prod = 1
         while len(res) < n:
-            res.append(i)
-            if i * 10 <= n:
-                i = i * 10
+            res.append(prod)
+            if prod * 10 <= n:
+                prod *= 10
             else:
-                while i >= n or i % 10 == 9:
-                    i = i // 10
-                i += 1
+                while prod >= n or prod % 10 == 9:
+                    prod //= 10
+                prod += 1
+
         return res
 
 
 sol = Solution()
+assert sol.lexicalOrder(13) == [1,10,11,12,13,2,3,4,5,6,7,8,9]
+assert sol.lexicalOrder(2) == [1,2]
+
+
+class Solution2:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+        def helper(prod):
+            if prod > n:
+                return
+            res.append(prod)
+            for i in range(0, 10):
+                next = prod * 10 + i
+                if next > n:
+                    break
+                helper(next)
+
+        for i in range(1, 10):
+            helper(i)
+        return res
+
+
+sol = Solution2()
 assert sol.lexicalOrder(13) == [1,10,11,12,13,2,3,4,5,6,7,8,9]
 assert sol.lexicalOrder(2) == [1,2]
