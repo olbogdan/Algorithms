b34931e78d3b9f81b56465fdde498e3504ef9b21
Alexander Bogdan <7153849+olbogdan@users.noreply.github.com>
2026-06-21 21:34:19 +0500

    feat: 1833. Maximum Ice Cream Bars

---
+++ Python/AlgoBootcamp/leetcode/easy/1833. Maximum Ice Cream Bars.py
@@ -0,0 +1,63 @@
+# It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+#
+# At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible.
+#
+# Note: The boy can buy the ice cream bars in any order.
+#
+# Return the maximum number of ice cream bars the boy can buy with coins coins.
+#
+# You must solve the problem by counting sort.
+#
+#
+#
+# Example 1:
+#
+# Input: costs = [1,3,2,4,1], coins = 7
+# Output: 4
+# Explanation: The boy can buy ice cream bars at indices 0,1,2,4 for a total price of 1 + 3 + 2 + 1 = 7.
+# Example 2:
+#
+# Input: costs = [10,6,8,7,7,8], coins = 5
+# Output: 0
+# Explanation: The boy cannot afford any of the ice cream bars.
+# Example 3:
+#
+# Input: costs = [1,6,3,1,2,5], coins = 20
+# Output: 6
+# Explanation: The boy can buy all the ice cream bars for a total price of 1 + 6 + 3 + 1 + 2 + 5 = 18.
+#
+#
+# Constraints:
+#
+# costs.length == n
+# 1 <= n <= 105
+# 1 <= costs[i] <= 105
+# 1 <= coins <= 108
+from typing import List
+
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        xMax = max(costs)
+        xMin = min(costs)
+
+        freq = [0] * (xMax + 1)
+        for x in costs:
+            freq[x] += 1
+
+        res = 0
+        for x in range(xMin, xMax + 1):
+            f = freq[x]
+            if f == 0:
+                continue
+            buy = min(coins // x, f)
+            if buy == 0:
+                break
+            res += buy
+            coins -= buy * x
+        return res
+
+
+sol = Solution()
+assert sol.maxIceCream([1,3,2,4,1], 7) == 4
+assert sol.maxIceCream([10,6,8,7,7,8], 5) == 0
